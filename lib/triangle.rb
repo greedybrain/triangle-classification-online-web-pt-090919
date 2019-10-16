4f8fcafe228de8552d1e3edde4d1b1efdb1d7c26
Naya  Willis <greedybrain18@gmail.com>
2019-10-16 02:02:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
-  # write code here
+  
+  attr_reader :side1, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
+  def kind
+    sides = [@side1, @side2, @side3]
+    if sides.all?{|side| side > 0}
+      :equilateral if sides.first == sides[1] && sides[1] == sides.last
+    end
+  end
+  
+  class TriangleError < StandardError
+    
+  end
+  
 end
