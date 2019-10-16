212f40d3481560e1b89da84b59551d62e43e94f2
Naya  Willis <greedybrain18@gmail.com>
2019-10-16 02:09:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
     sides = [@side1, @side2, @side3]
     # binding.pry
     return :equilateral if sides.first == sides[1] && sides[1] == sides.last
-    return :isosceles if sides[1] == sides.last || sides.first == sides.last
+    return :isosceles if sides[1] == sides.last || sides.first == sides.last || sides.first == sides[1]
   end
   
 end
