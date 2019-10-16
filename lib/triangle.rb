f183c33bdaf297553bc109ba913ea5fa976405a8
Naya  Willis <greedybrain18@gmail.com>
2019-10-16 02:16:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,13 +8,13 @@ class Triangle
     @side1 = side1
     @side2 = side2
     @side3 = side3
-    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
-      raise TriangleError
-    end
   end
   
   def kind
     sides = [@side1, @side2, @side3]
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+      raise TriangleError
+    end
     # binding.pry
     return :equilateral if sides.first == sides[1] && sides[1] == sides.last
     return :isosceles if sides[1] == sides.last || sides.first == sides.last || sides.first == sides[1]
